7c72882d3e8c3c3867c5d924feb81f8f29e484cc
PlaidPeacoat <114315812+PlaidPeacoat@users.noreply.github.com>
2023-02-06 18:57:55 -0500

    2.8 changes

--- index.js
+++ index.js
@@ -11,8 +11,6 @@ const mongoose = require('mongoose');
 const Models = require('./models.js');
 const Movies = Models.Movie;
 const Users = Models.User;
-const Directors = Models.Director;
-const Genres = Models.Genre;
 mongoose.connect('mongodb://localhost:27017/myflixDB', { useNewUrlParser: true, useUnifiedTopology: true });
 
 
@@ -29,49 +27,57 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 /* res.send object replaces response.writeHead and response.end code */
 app.get('/', (req, res) => {
-  res.send('Welcome to MyFlix Movie App!');
+  res.send('Welcome to MyFlix!');
 });
 
+
 /* 1. GETs list of all movies */
-app.get('/movies', (req, res) => {
-    res.status(200).json(movies);
+
+  app.get('/movies', (req, res) => {
+    Movies.find()
+    .then((movies) => {
+      res.status(201).json(movies);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).send('Error: ' + err);
+    });
   });
-
 /* 2. GETs specific movie data by title */
-app.get('/movies/:title', (req, res) => {
-  const { title } = req.params;
-  const movie = movies.find( movie => movie.title === title );
+// app.get('/movies/:title', (req, res) => {
+//   const { title } = req.params;
+//   const movie = movies.find( movie => movie.title === title );
 
-  if (movie) {
-    res.status(200).json(movie);
-  } else {
-    res.status(400).send('this movie doesn\'t exist!')
-  }
-})
+//   if (movie) {
+//     res.status(200).json(movie);
+//   } else {
+//     res.status(400).send('this movie doesn\'t exist!')
+//   }
+// })
 
-/* 3. GETs specific movie data by genre */
-app.get('/movies/genre/:genreName', (req, res) => {
-  const { genreName } = req.params;
-  const genre = movies.find( movie => movie.genre.name === genreName ).genre;
+// /* 3. GETs specific movie data by genre */
+// app.get('/movies/genre/:genreName', (req, res) => {
+//   const { genreName } = req.params;
+//   const genre = movies.find( movie => movie.genre.name === genreName ).genre;
 
-  if (genre) {
-    res.status(200).json(genre);
-  } else {
-    res.status(400).send('this genre doesn\'t exist!')
-  }
-})
+//   if (genre) {
+//     res.status(200).json(genre);
+//   } else {
+//     res.status(400).send('this genre doesn\'t exist!')
+//   }
+// })
 
-/* 4. GETs specific movie data by director */
-app.get('/movies/director/:directorName', (req, res) => {
-  const { directorName } = req.params;
-  const director = movies.find( movie => movie.director.name === directorName ).director;
+// /* 4. GETs specific movie data by director */
+// app.get('/movies/director/:directorName', (req, res) => {
+//   const { directorName } = req.params;
+//   const director = movies.find( movie => movie.director.name === directorName ).director;
 
-  if (director) {
-    res.status(200).json(director);
-  } else {
-    res.status(400).send('this director doesn\'t exist!')
-  }
-})
+//   if (director) {
+//     res.status(200).json(director);
+//   } else {
+//     res.status(400).send('this director doesn\'t exist!')
+//   }
+// })
 
 /* 5. POST: allows new users to register  */
 app.post('/users', (req, res) => {
@@ -101,72 +107,82 @@ app.post('/users', (req, res) => {
 });
 
 /* 6. PUT: allows user to update their userinfo */
+app.get('/users', (req, res) => {
+  Users.find()
+    .then((users) => {
+      res.status(201).json(users);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    });
+});
 
-app.put('/users/:id', (req, res) => {
-  const { id } = req.params;
-  const updateUser = req.body;
-  /* only use 2 = rather than 3 = as user.id is a number but id is  a string */
-  let user = users.find( user => user.id == id );
+// app.put('/users/:id', (req, res) => {
+//   const { id } = req.params;
+//   const updateUser = req.body;
+//   /* only use 2 = rather than 3 = as user.id is a number but id is  a string */
+//   let user = users.find( user => user.id == id );
 
-  if (user) {
-    user.name = updateUser.name;
-    res.status(200).json(user);
-  } else {
-    res.status(400).send('user name not found')
+//   if (user) {
+//     user.name = updateUser.name;
+//     res.status(200).json(user);
+//   } else {
+//     res.status(400).send('user name not found')
 
-  }
-})
+//   }
+// })
 
-/* 7. POST: allow users to add a movie to their favourites   */
+// /* 7. POST: allow users to add a movie to their favourites   */
 
-app.post('/users/:id/:movieTitle', (req, res) => {
-  const { id, movieTitle } = req.params;
+// app.post('/users/:id/:movieTitle', (req, res) => {
+//   const { id, movieTitle } = req.params;
   
-  /* only use 2 = rather than 3 = as user.id is a number but id is  a string */
-  let user = users.find( user => user.id == id );
+//   /* only use 2 = rather than 3 = as user.id is a number but id is  a string */
+//   let user = users.find( user => user.id == id );
 
-  if (user) {
-    user.favoriteMovie.push(movieTitle);
-    res.status(200).send(`${movieTitle} has been added to user ${id}'s array`);
-  } else {
-    res.status(400).send('user name not found')
+//   if (user) {
+//     user.favoriteMovie.push(movieTitle);
+//     res.status(200).send(`${movieTitle} has been added to user ${id}'s array`);
+//   } else {
+//     res.status(400).send('user name not found')
 
-  }
-})
+//   }
+// })
 
-/* 8. DELETE: allow users to remove a movie from their favourites */
+// /* 8. DELETE: allow users to remove a movie from their favourites */
 
-app.delete('/users/:id/:movieTitle', (req, res) => {
-  const { id, movieTitle } = req.params;
+// app.delete('/users/:id/:movieTitle', (req, res) => {
+//   const { id, movieTitle } = req.params;
   
-  /* only use 2 = rather than 3 = as user.id is a number but id is  a string */
-  let user = users.find( user => user.id == id );
+//   /* only use 2 = rather than 3 = as user.id is a number but id is  a string */
+//   let user = users.find( user => user.id == id );
 
-  if (user) {
-    user.favoriteMovie = user.favoriteMovie.filter( title => title !== movieTitle )
-    res.status(200).send(`${movieTitle} has been removed from user ${id}'s array`);
-  } else {
-    res.status(400).send('user name not found')
+//   if (user) {
+//     user.favoriteMovie = user.favoriteMovie.filter( title => title !== movieTitle )
+//     res.status(200).send(`${movieTitle} has been removed from user ${id}'s array`);
+//   } else {
+//     res.status(400).send('user name not found')
 
-  }
-})
+//   }
+// })
 
-/* 9. DELETE: allow users to de-register */
+// /* 9. DELETE: allow users to de-register */
 
-app.delete('/users/:id', (req, res) => {
-  const { id } = req.params;
+// app.delete('/users/:id', (req, res) => {
+//   const { id } = req.params;
   
-  /* only use 2 = rather than 3 = as user.id is a number but id is  a string */
-  let user = users.find( user => user.id == id );
+//   /* only use 2 = rather than 3 = as user.id is a number but id is  a string */
+//   let user = users.find( user => user.id == id );
 
-  if (user) {
-    users = users.filter( user => user.id != id )
-    res.status(200).send(` user ${id} has been successfully deleted `);
-  } else {
-    res.status(400).send('user name not found')
+//   if (user) {
+//     users = users.filter( user => user.id != id )
+//     res.status(200).send(` user ${id} has been successfully deleted `);
+//   } else {
+//     res.status(400).send('user name not found')
 
-  }
-})
+//   }
+// })
 
 
 
