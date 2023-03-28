47dfa7805b45eb94bced6b41eeb4337d8dfab179
PlaidPeacoat <114315812+PlaidPeacoat@users.noreply.github.com>
2023-03-28 13:40:03 -0400

    added netlify to allowedOrigins array

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 const cors = require('cors');
-let allowedOrigins = ['http://localhost:8080', 'http://testsite.com'];
+let allowedOrigins = ['http://localhost:8080', 'http://testsite.com', 'https://myflix-a-sears.netlify.app'];
 
 app.use(cors({
   origin: (origin, callback) => {
