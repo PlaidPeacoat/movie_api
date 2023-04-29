18d1916a33f9ab27761e3d3e48fca855fe995ab1
PlaidPeacoat <114315812+PlaidPeacoat@users.noreply.github.com>
2023-04-29 14:08:34 -0400

    commented cors to narrow down issue

--- index.js
+++ index.js
@@ -14,21 +14,22 @@ let allowedOrigins = [
   'https://myflix-a-sears.netlify.app',
 ];
 
-app.use(
-  cors({
-    origin: (origin, callback) => {
-      if (!origin) return callback(null, true);
-      if (allowedOrigins.indexOf(origin) === -1) {
-        // If a specific origin isn’t found on the list of allowed origins
-        let message =
-          'The CORS policy for this application doesn’t allow access from origin ' +
-          origin;
-        return callback(new Error(message), false);
-      }
-      return callback(null, true);
-    },
-  })
-);
+// app.use(
+//   cors({
+//     origin: (origin, callback) => {
+//       if (!origin) return callback(null, true);
+//       if (allowedOrigins.indexOf(origin) === -1) {
+//         // If a specific origin isn’t found on the list of allowed origins
+//         let message =
+//           'The CORS policy for this application doesn’t allow access from origin ' +
+//           origin;
+//         return callback(new Error(message), false);
+//       }
+//       return callback(null, true);
+//     },
+//   })
+// );
+app.use(cors());
 
 // Authentication (auth.js is handling login endpoint and generating JWT tokens)
 let auth = require('./auth.js')(app);
@@ -47,9 +48,9 @@ const Users = Models.User;
 
 // Suppresses deprecation warning
 mongoose.set('strictQuery', true);
-mongoose.connect(process.env.CONNECTION_URI);
+//mongoose.connect(process.env.CONNECTION_URI);
 // Local database
-//mongoose.connect('mongodb://localhost:27017/myFlixDB');
+mongoose.connect('mongodb://localhost:27017/myFlixDB');
 
 //Routing of static files (e.g. documentation.html)
 app.use(express.static('public'));
